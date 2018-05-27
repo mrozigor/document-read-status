53f26d64a7b358ede2aa7df9d93994c136e7c8e8
Igor Mróz <mrozigor@wp.pl>
2018-05-27 21:51:16 +0200

    Initial commit. Basic implementation. Needs to handle errors that might occur (e.g. window narrower than file name).

---
+++ README.md
@@ -0,0 +1,11 @@
+INFO
+==========
+Simple script which allows to track file status (read/not read). Created for ebooks library, because ereader's built-in software sometimes magically reset 'read' status. As for now it handles only basic options and error checks.
+
+USAGE
+==========
+Keys are described on status bar. You enter path to library then reload it. Script creates database in library location. Then with keys up/down you can select file and mark it read/unread.
+
+LICENSE
+==========
+WTFPLv2
---
+++ main.py
@@ -0,0 +1,235 @@
+#!/bin/env python3
+
+import sys
+import curses
+import os
+import os.path
+import sqlite3
+from functools import reduce
+
+def draw(stdscr):
+    characterPressed = 0
+    items = []
+    position = 0
+    startListPosition = 0
+    endListPosition = 0
+    readItems = 0
+    libraryPath = ""
+    changeLibraryPathMode = False
+
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
+    curses.curs_set(0)
+
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    while True:
+        if characterPressed == 27:
+            changeLibraryPathMode = False
+
+        if changeLibraryPathMode:
+            if characterPressed == curses.KEY_BACKSPACE:
+                libraryPath = libraryPath[:-1]
+            else:
+                libraryPath += chr(characterPressed)
+        else:
+            if (characterPressed == curses.KEY_DOWN) and (len(items) != 0):
+                position = ((position + 1) % len(items))
+                if position > endListPosition:
+                    startListPosition = startListPosition + 1
+                    endListPosition = endListPosition + 1
+                if position < startListPosition:
+                    startListPosition = position
+                    endListPosition = position
+            elif (characterPressed == curses.KEY_UP) and (len(items) != 0):
+                position = ((position - 1) % len(items))
+                if position < startListPosition:
+                    startListPosition = startListPosition - 1
+                    endListPosition = endListPosition - 1
+                if position > endListPosition:
+                    startListPosition = position
+                    endListPosition = position
+            elif characterPressed == ord('x'):
+                break
+            elif characterPressed == ord('r'):
+                if os.path.isdir(libraryPath):
+                    stdscr.addstr(1, 1, libraryPathHeader + "  Reloading...")
+                    stdscr.refresh()
+                    if libraryPath[len(libraryPath) - 1] != "/":
+                        libraryPath = libraryPath + "/"
+
+                    if not checkIfDatabaseExists(libraryPath):
+                        createDatabase(libraryPath)
+
+                    databaseItems = getItemsFromDatabase(libraryPath)
+                    insertStatements = []
+
+                    for filePath in listFiles(libraryPath):
+                        found = False
+
+                        for databaseItem in databaseItems:
+                            if databaseItem[1] == filePath:
+                                found = True
+                                break
+
+                        if not found:
+                            insertStatements += [["INSERT INTO documents (relative_path, is_read) VALUES (?, 0)", filePath]]
+
+                    insertData(libraryPath, insertStatements)
+
+                    items = getItemsFromDatabase(libraryPath)
+                    readItems = reduce((lambda a, b: a + b), list(map(lambda x: x[2], items)))
+
+                    # TODO/FUTURE CHECK IF FILE WITH SAME NAME IN DB AND ON DISK STILL EXISTS ON SAME PATH AS IN DB (IF NO THEN ASK IF REPLACE PATH IN DB)
+            elif characterPressed == ord('c'):
+                changeLibraryPathMode = True
+            elif characterPressed == ord(' '):
+                if len(items) > 0:
+                    items[position] = (items[position][0], items[position][1], int(not items[position][2]))
+                    updateDocument(libraryPath, items[position])
+                    readItems = reduce((lambda a, b: a + b), list(map(lambda x: x[2], items)))
+
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        if len(items) > 0:
+            if (height - 6) != (endListPosition - startListPosition):
+                endListPosition = (startListPosition + (height - 6))
+                if position > endListPosition:
+                    endListPosition = position
+                    startListPosition = endListPosition - (height - 6)
+            if endListPosition >= len(items):
+                endListPosition = len(items) - 1
+                startListPosition = endListPosition - (height - 6)
+
+        libraryPathHeader = "Path to library: " + libraryPath
+        stdscr.addstr(1, 1, libraryPathHeader)
+        if changeLibraryPathMode:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(1, 1 + len(libraryPathHeader), " ")
+            stdscr.attroff(curses.color_pair(1))
+
+        # Items list
+        itemsListHeader = " Number | Is read? | Path"
+        stdscr.attron(curses.color_pair(1))
+        stdscr.addstr(3, 0, itemsListHeader)
+        stdscr.addstr(3, len(itemsListHeader), " " * (width - len(itemsListHeader) - 1))
+        stdscr.attroff(curses.color_pair(1))
+        if len(items) > 0:
+            drawItems(stdscr, items[startListPosition:(endListPosition + 1)], items[position], startListPosition)
+
+        # Status bar
+        statusbarstr = " e(x)it | (r)eload | (c)hange library path / (ESC) return | (SPC) mark read/unread | Read items: {}/{}".format(readItems, len(items))
+        stdscr.attron(curses.color_pair(1))
+        stdscr.addstr(height - 1, 0, statusbarstr)
+        stdscr.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        stdscr.attroff(curses.color_pair(1))
+
+        # Refresh the screen
+        stdscr.refresh()
+        # Wait for next input
+        characterPressed = stdscr.getch()
+
+def main():
+    curses.wrapper(draw)
+
+def listFiles(directory):
+    filesList = []
+    includedExtensions = [".pdf", ".doc", ".docx", ".odt", ".mobi", ".epub"]
+
+    if os.path.isdir(directory):
+        for path, subdirs, files in os.walk(directory):
+            for name in files:
+                add = False
+
+                if name == "database.db":
+                    continue
+
+                for extension in includedExtensions:
+                    if name.lower().endswith(extension):
+                        add = True
+                        break
+
+                if add:
+                    filesList += [os.path.join(path, name).replace(directory, "")]
+
+    return filesList
+
+def createDatabase(directory):
+    result = False
+
+    try:
+        connection = sqlite3.connect(directory + "database.db")
+        connection.cursor().execute("CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY, relative_path text NOT NULL UNIQUE, is_read INTEGER NOT NULL)")
+        result = True
+    except Exception:
+        pass
+    finally:
+        connection.close()
+
+    return result
+
+def checkIfDatabaseExists(directory):
+    return os.path.isfile(directory + "database.db")
+
+def getItemsFromDatabase(directory):
+    results = []
+
+    try:
+        connection = sqlite3.connect(directory + "database.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM documents ORDER BY relative_path ASC")
+        results = cursor.fetchall()
+    except Exception:
+        pass
+    finally:
+        connection.close()
+
+    return results
+
+def insertData(directory, insertStatements):
+    result = False
+
+    try:
+        connection = sqlite3.connect(directory + "database.db")
+        for statement in insertStatements:
+            connection.cursor().execute(statement[0], [statement[1]])
+        connection.commit()
+        result = True
+    except Exception:
+        pass
+    finally:
+        connection.close()
+
+    return result
+
+def drawItems(screen, items, selectedItem, startPosition):
+    index = 0
+    for item in items:
+        if item == selectedItem:
+            screen.attron(curses.color_pair(1))
+        screen.addstr(index + 4, 0, "{:^8}|{:^10}| {}".format(startPosition + index, ("*" if item[2] == 1 else ""), item[1]))
+        if item == selectedItem:
+            screen.attroff(curses.color_pair(1))
+        index = index + 1
+
+def updateDocument(directory, document):
+    result = False
+
+    try:
+        connection = sqlite3.connect(directory + "database.db")
+        connection.cursor().execute("UPDATE documents SET is_read = ? WHERE id = ?", [document[2], document[0]])
+        connection.commit()
+        result = True
+    except Exception:
+        pass
+    finally:
+        connection.close()
+
+    return result
+
+if __name__ == "__main__":
+    main()
