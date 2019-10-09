9990ca9047ab6f52275dfac750147e60e21b1206
Igor Mróz <mrozigor@wp.pl>
2019-10-09 20:30:41 +0200

    WiP. Refactor in progress. Main method looks new better ;) App after loading library still doesn't work - it will be fixed next time.

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ class View:
     ITEMS_LIST_HEADER = " Number | Is read? | Path"
 
     def __init__(self, screen):
+        self.libraryPath = ""
         self.screen = screen
-        self.initializeCurses()
-        self.createPanels()
+        self._initializeCurses()
+        self._createPanels()
 
-    def initializeCurses(self):
+    def _initializeCurses(self):
         self.screen.clear()
         self.screen.refresh()
         curses.curs_set(0)
@@ -26,7 +27,7 @@ class View:
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
         curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
 
-    def createPanels(self):
+    def _createPanels(self):
         self.windows = []
         self.panels = []
 
@@ -47,30 +48,42 @@ class View:
         self.panels[2].hide()
         self.panels[3].hide()
 
+    def _drawItems(items, selectedItem, startPosition):
+        index = 1
+        for item in items:
+            if item == selectedItem:
+                self.screen.attron(curses.color_pair(1))
+            self.screen.addstr(index, 0, "{:^8}|{:^10}| {}".format(startPosition + index, ("*" if item[3] == 1 else ""), item[1]))
+            if item == selectedItem:
+                self.screen.attroff(curses.color_pair(1))
+            index = index + 1
+
     def getPressedCharacter(self):
         return self.screen.getch()
 
-    def drawScreen(self, items, startListPosition, endListPosition, position, readItems, libraryPath):
+    def drawScreen(self, items, startListPosition, endListPosition, position, readItems):
+        self.screen.clear()
         self.height, self.width = self.screen.getmaxyx()
+        
         # Items list
         self.screen.attron(curses.color_pair(1))
         self.screen.addstr(0, 0, self.ITEMS_LIST_HEADER)
         self.screen.addstr(0, len(self.ITEMS_LIST_HEADER), " " * (self.width - len(self.ITEMS_LIST_HEADER) - 1))
         self.screen.attroff(curses.color_pair(1))
         if len(items) > 0:
-            drawItems(self.screen, items[startListPosition:(endListPosition + 1)], items[position], startListPosition)
+            self._drawItems(items[startListPosition:(endListPosition + 1)], items[position], startListPosition)
 
         # Status bar
         statusBar = self.STATUS_BAR.format(readItems, len(items))
         self.screen.attron(curses.color_pair(1))
         self.screen.addstr(self.height - 1, 0, statusBar)
-        self.screen.addstr(self.height - 1, len(statusBar), ("library: " + libraryPath + " ").rjust((self.width - len(statusBar) - 1), " "))
+        self.screen.addstr(self.height - 1, len(statusBar), ("library: " + self.libraryPath + " ").rjust((self.width - len(statusBar) - 1), " "))
         self.screen.attroff(curses.color_pair(1))
 
         curses.panel.update_panels()
         self.screen.refresh()
 
-    def showLibrarySelectionDialog(self, currentLibraryPath):
+    def showLibrarySelectionDialog(self):
         window = curses.newwin(3, int(self.width - 0.5 * self.width), int(self.height - 0.5 * self.height - 2), int(self.width - 0.75 * self.width))
         window.attron(curses.color_pair(2))
         window.box()
@@ -84,15 +97,15 @@ class View:
 
         while characterPressed != 10:
             if characterPressed == curses.KEY_BACKSPACE:
-                currentLibraryPath = currentLibraryPath[:-1]
+                self.libraryPath = self.libraryPath[:-1]
             else:
                 try:
-                    currentLibraryPath += chr(characterPressed)
+                    self.libraryPath += chr(characterPressed)
                 except ValueError:
                     pass
 
             self.windows[0].attron(curses.color_pair(2))
-            self.windows[0].addstr(1, 1, "Path to library: " + currentLibraryPath.ljust(int(self.width - 0.5 * self.width - 19), " "))
+            self.windows[0].addstr(1, 1, "Path to library: " + self.libraryPath.ljust(int(self.width - 0.5 * self.width - 19), " "))
             self.windows[0].attroff(curses.color_pair(2))
             curses.panel.update_panels() # TODO IS THIS REALLY NECESSARY?
             self.screen.refresh()
@@ -102,8 +115,6 @@ class View:
         self.panels[0].hide()
         curses.panel.update_panels()
         self.screen.refresh()
-
-        return currentLibraryPath
 
     def showReloadLibraryDialog(self):
         window = curses.newwin(7, 24, int(self.height - 0.5 * self.height - 4), int(self.width / 2 - 12))
@@ -150,10 +161,20 @@ class View:
         self.panels[2].hide()
         self.screen.refresh()
 
+    def getLibraryPath(self):
+        return self.libraryPath
+
 class Controller:
     DATABASE_FILE_NAME = "database.db"
     CREATE_TABLE_STATEMENT = "CREATE TABLE IF NOT EXISTS documents (content_hash text PRIMARY KEY, relative_path text NOT NULL UNIQUE, is_read INTEGER NOT NULL)"
     SELECT_ALL_STATEMENT = "SELECT * FROM documents ORDER BY relative_path ASC"
+    INSERT_STATEMENT = "INSERT INTO documents (relative_path, content_hash, is_read) VALUES (:relative_path, :content_hash, 0)"
+    DELETE_STATEMENT = "DELETE FROM documents WHERE content_hash=:content_hash"
+    UPDATE_PATH_STATEMENT = "UPDATE documents SET relative_path=:relative_path WHERE content_hash=:content_hash"
+    UPDATE_READ_STATUS_STATEMENT = "UPDATE documents SET is_read=:is_read WHERE id=:id"
+
+    def __init__(self):
+        self.items = []
 
     def _checkIfDatabaseExists(self, path):
         return os.path.isfile(path)
@@ -175,11 +196,11 @@ class Controller:
     def _pathEndsWithSlash(self, path):
         return path.endswith("/")
 
-    def _loadItemsFromDatabase(self, path):
+    def _loadItemsFromDatabase(self, databasePath):
         results = []
 
         try:
-            connection = sqlite3.connect(path)
+            connection = sqlite3.connect(databasePath)
             cursor = connection.cursor()
             cursor.execute(self.SELECT_ALL_STATEMENT)
             results = cursor.fetchall()
@@ -223,6 +244,24 @@ class Controller:
 
         return filesList, duplicatedFiles
 
+    def _executeSql(self, sqlStatements):
+        result = True
+
+        try:
+            connection = sqlite3.connect(self.databasePath)
+
+            for statement in sqlStatements:
+                connection.cursor().execute(statement[0], statement[1])
+
+            connection.commit()
+        except Exception:
+            result = False
+            pass
+        finally:
+            connection.close()
+
+        return result
+
     def isDirectory(self, path):
         return os.path.isdir(path)
 
@@ -230,19 +269,17 @@ class Controller:
         if not self._pathEndsWithSlash(libraryPath):
             libraryPath = libraryPath + "/"
 
-        databasePath = libraryPath + self.DATABASE_FILE_NAME
+        self.databasePath = libraryPath + self.DATABASE_FILE_NAME
 
-        if not self._checkIfDatabaseExists(databasePath):
-            self._createDatabase(databasePath)
+        if not self._checkIfDatabaseExists(self.databasePath):
+            self._createDatabase(self.databasePath)
 
-        databaseItems = self._loadItemsFromDatabase(databasePath)
+        databaseItems = self._loadItemsFromDatabase(self.databasePath)
         filesData, duplicatedFiles  = self._listFiles(libraryPath)
 
         databaseHashes = {}
-        items = {} # {"fileHash": [False, False]} - first one if it exists in fileHashes, second if in database
-        insertStatements = [] # TODO CHECKPOINT
-        deleteStatements = []
-        updateStatements = []
+        items = {} # {"fileHash" => [False, False]} - first one if hash exists in files, second if in database
+        sqlStatements = []
 
         for fileHash, filePath in filesData.items():
             items[fileHash] = [True, False]
@@ -259,181 +296,110 @@ class Controller:
 
         for fileHash, info in items.items():
             if info[0] and not info[1]:
-                insertStatements += [["INSERT INTO documents (relative_path, content_hash, is_read) VALUES (?, ?, 0)", filesData[fileHash], fileHash]]
+                sqlStatements += [[self.INSERT_STATEMENT, {"relative_path": filesData[fileHash], "content_hash": fileHash}]]
                 addedItems += 1
 
             if not info[0] and info[1]:
-                deleteStatements += [["DELETE FROM documents WHERE content_hash=?", fileHash]]
+                sqlStatements += [[self.DELETE_STATEMENT, {"content_hash": fileHash}]]
                 removedItems += 1
 
             if info[0] and info[1]:
                 if filesData[fileHash] != databaseHashes[fileHash]:
-                    updateStatements += [["UPDATE documents SET relative_path=? WHERE content_hash=?", filesData[fileHash], fileHash]]
+                    sqlStatements += [[self.UPDATE_PATH_STATEMENT, {"relative_path": filesData[fileHash], "content_hash": fileHash}]]
                     movedItems += 1
 
-        insertData(libraryPath, insertStatements)
-        deleteData(libraryPath, deleteStatements)
-        updateData(libraryPath, updateStatements)
+        self._executeSql(sqlStatements)
 
-        self.items = self._loadItemsFromDatabase(databasePath)
+        self.items = self._loadItemsFromDatabase(self.databasePath)
 
         return self.items, addedItems, removedItems, movedItems
 
+    def getNumberOfItems(self):
+        return len(self.items)
+
+    def getItems(self):
+        return self.items
+
     def getReadItems(self):
-        return reduce((lambda a, b: a + b), list(map(lambda x: x[3], self.items)))
+        return 0 if not self.items else reduce((lambda a, b: a + b), list(map(lambda x: x[3], self.items)))
+
+    def changeReadState(self, position):
+        if len(self.items) <= 0:
+            return # TODO MAYBE CHECK OTHER WAY IF POSITION IS CORRECT?
+        
+        self.items[position][3] = int(not self.items[position][3])
+        sqlStatement = [[self.UPDATE_READ_STATUS_STATEMENT, {"is_read": self.items[position][3], "id": self.items[position][0]}]]
+        self._executeSql(sqlStatement)
 
 def main(screen):
     view = View(screen)
     controller = Controller()
     characterPressed = 0
-    items = []
     position = 0
     startListPosition = 0
     endListPosition = 0
-    readItems = 0
-    libraryPath = ""
 
     while characterPressed != ord('x'):
-        if True: #TODO REMOVE AND CHANGE INDENT
-            if (characterPressed == curses.KEY_DOWN) and (len(items) != 0):
-                position = ((position + 1) % len(items))
-                if position > endListPosition:
-                    startListPosition = startListPosition + 1
-                    endListPosition = endListPosition + 1
-                if position < startListPosition:
-                    startListPosition = position
-                    endListPosition = position
-            elif (characterPressed == curses.KEY_UP) and (len(items) != 0):
-                position = ((position - 1) % len(items))
-                if position < startListPosition:
-                    startListPosition = startListPosition - 1
-                    endListPosition = endListPosition - 1
-                if position > endListPosition:
-                    startListPosition = position
-                    endListPosition = position
-            elif (characterPressed == curses.KEY_RIGHT) and (len(items) != 0):
-                delta = endListPosition - startListPosition
-                if (endListPosition + delta) > len(items):
-                    endListPosition = len(items) - 1
-                    startListPosition = endListPosition - delta
-                    position = endListPosition
-                else:
-                    startListPosition = startListPosition + delta
-                    endListPosition = endListPosition + delta
-                    position = startListPosition
-            elif (characterPressed == curses.KEY_LEFT) and (len(items) != 0):
-                delta = endListPosition - startListPosition
-                if (startListPosition - delta) < 0:
-                    startListPosition = 0
-                    endListPosition = startListPosition + delta
-                else:
-                    startListPosition = startListPosition - delta
-                    endListPosition = endListPosition - delta
+        if (characterPressed == curses.KEY_DOWN) and (controller.getNumberOfItems() != 0):
+            position = ((position + 1) % controller.getNumberOfItems())
+            if position > endListPosition:
+                startListPosition = startListPosition + 1
+                endListPosition = endListPosition + 1
+            if position < startListPosition:
+                startListPosition = position
+                endListPosition = position
+        elif (characterPressed == curses.KEY_UP) and (controller.getNumberOfItems() != 0):
+            position = ((position - 1) % controller.getNumberOfItems())
+            if position < startListPosition:
+                startListPosition = startListPosition - 1
+                endListPosition = endListPosition - 1
+            if position > endListPosition:
+                startListPosition = position
+                endListPosition = position
+        elif (characterPressed == curses.KEY_RIGHT) and (controller.getNumberOfItems() != 0):
+            delta = endListPosition - startListPosition
+            if (endListPosition + delta) > controller.getNumberOfItems():
+                endListPosition = controller.getNumberOfItems() - 1
+                startListPosition = endListPosition - delta
+                position = endListPosition
+            else:
+                startListPosition = startListPosition + delta
+                endListPosition = endListPosition + delta
                 position = startListPosition
-            elif characterPressed == ord('r'):
-                if controller.isDirectory(libraryPath):
-                    view.showReloadLibraryDialog()
-                    [items, addedItems, removedItems, movedItems] = controller.loadItems(libraryPath)
-                    readItems = controller.getReadItems()
-                    view.hideReloadLibraryDialog()
-                    view.showReloadSummaryDialog(addedItems, removedItems, movedItems)
-            elif characterPressed == ord('c'):
-                libraryPath = view.showLibrarySelectionDialog(libraryPath)
-            elif characterPressed == ord(' '):
-                if len(items) > 0:
-                    items[position] = (items[position][0], items[position][1], items[position][2], int(not items[position][3]))
-                    updateDocument(libraryPath, items[position])
-                    readItems = reduce((lambda a, b: a + b), list(map(lambda x: x[3], items)))
+        elif (characterPressed == curses.KEY_LEFT) and (controller.getNumberOfItems() != 0):
+            delta = endListPosition - startListPosition
+            if (startListPosition - delta) < 0:
+                startListPosition = 0
+                endListPosition = startListPosition + delta
+            else:
+                startListPosition = startListPosition - delta
+                endListPosition = endListPosition - delta
+            position = startListPosition
+        elif characterPressed == ord('r'):
+            if controller.isDirectory(view.getLibraryPath()):
+                view.showReloadLibraryDialog()
+                [items, addedItems, removedItems, movedItems] = controller.loadItems(view.getLibraryPath())
+                view.hideReloadLibraryDialog()
+                view.showReloadSummaryDialog(addedItems, removedItems, movedItems)
+        elif characterPressed == ord('c'):
+            view.showLibrarySelectionDialog()
+        elif characterPressed == ord(' '):
+            controller.changeReadState(position)
 
-        if len(items) > 0:
+        if controller.getNumberOfItems() > 0:
             if (height - 2) != (endListPosition - startListPosition):
                 endListPosition = (startListPosition + (height - 3))
                 if position > endListPosition:
                     endListPosition = position
                     startListPosition = endListPosition - (height - 3)
-            if endListPosition >= len(items):
-                endListPosition = len(items) - 1
+            if endListPosition >= controller.getNumberOfItems():
+                endListPosition = controller.getNumberOfItems() - 1
                 startListPosition = endListPosition - (height - 3)
 
-        screen.clear()
-
-        view.drawScreen(items, startListPosition, endListPosition, position, readItems, libraryPath)
+        view.drawScreen(controller.getItems(), startListPosition, endListPosition, position, controller.getReadItems())
 
         # Wait for next input
         characterPressed = view.getPressedCharacter()
-
-def insertData(directory, insertStatements):
-    result = False
-
-    try:
-        connection = sqlite3.connect(directory + "database.db")
-        for statement in insertStatements:
-            connection.cursor().execute(statement[0], [statement[1], statement[2]])
-        connection.commit()
-        result = True
-    except Exception:
-        pass
-    finally:
-        connection.close()
-
-    return result
-
-def deleteData(directory, deleteStatements):
-    result = False
-
-    try:
-        connection = sqlite3.connect(directory + "database.db")
-        for statement in deleteStatements:
-            connection.cursor().execute(statement[0], [statement[1]])
-        connection.commit()
-        result = True
-    except Exception:
-        pass
-    finally:
-        connection.close()
-
-    return result
-
-def updateData(directory, updateStatements):
-    result = False
-
-    try:
-        connection = sqlite3.connect(directory + "database.db")
-        for statement in updateStatements:
-            connection.cursor().execute(statement[0], [statement[1], statement[2]])
-        connection.commit()
-        result = True
-    except Exception:
-        pass
-    finally:
-        connection.close()
-
-    return result
-
-def drawItems(screen, items, selectedItem, startPosition):
-    index = 1
-    for item in items:
-        if item == selectedItem:
-            screen.attron(curses.color_pair(1))
-        screen.addstr(index, 0, "{:^8}|{:^10}| {}".format(startPosition + index, ("*" if item[3] == 1 else ""), item[1]))
-        if item == selectedItem:
-            screen.attroff(curses.color_pair(1))
-        index = index + 1
-
-def updateDocument(directory, document):
-    result = False
-
-    try:
-        connection = sqlite3.connect(directory + "database.db")
-        connection.cursor().execute("UPDATE documents SET is_read = ? WHERE id = ?", [document[3], document[0]])
-        connection.commit()
-        result = True
-    except Exception:
-        pass
-    finally:
-        connection.close()
-
-    return result
 
 if __name__ == "__main__":
     curses.wrapper(main)
