010737cb05a7c70e335399613ef0a7e9c870df78
Igor Mróz <mrozigor@wp.pl>
2019-10-14 22:37:21 +0200

    Moved classes into separate files.

---
+++ controller.py
@@ -0,0 +1,171 @@
+import os, os.path
+import sqlite3
+import hashlib
+from functools import reduce
+
+class Controller:
+    DATABASE_FILE_NAME = "database.db"
+    CREATE_TABLE_STATEMENT = "CREATE TABLE IF NOT EXISTS documents (content_hash text PRIMARY KEY, relative_path text NOT NULL UNIQUE, is_read INTEGER NOT NULL)"
+    SELECT_ALL_STATEMENT = "SELECT * FROM documents ORDER BY relative_path ASC"
+    INSERT_STATEMENT = "INSERT INTO documents (relative_path, content_hash, is_read) VALUES (:relative_path, :content_hash, 0)"
+    DELETE_STATEMENT = "DELETE FROM documents WHERE content_hash=:content_hash"
+    UPDATE_PATH_STATEMENT = "UPDATE documents SET relative_path=:relative_path WHERE content_hash=:content_hash"
+    UPDATE_READ_STATUS_STATEMENT = "UPDATE documents SET is_read=:is_read WHERE id=:id"
+
+    def __init__(self):
+        self.items = []
+
+    def _checkIfDatabaseExists(self, path):
+        return os.path.isfile(path)
+
+    def _createDatabase(self, path):
+        result = False
+
+        try:
+            connection = sqlite3.connect(path)
+            connection.cursor().execute(self.CREATE_TABLE_STATEMENT)
+            result = True
+        except Exception:
+            pass
+        finally:
+            connection.close()
+
+        return result
+
+    def _pathEndsWithSlash(self, path):
+        return path.endswith("/")
+
+    def _loadItemsFromDatabase(self, databasePath):
+        results = []
+
+        try:
+            connection = sqlite3.connect(databasePath)
+            cursor = connection.cursor()
+            cursor.execute(self.SELECT_ALL_STATEMENT)
+            results = cursor.fetchall()
+        except Exception:
+            pass
+        finally:
+            connection.close()
+
+        return results
+
+    def _listFiles(self, path):
+        HASH_BUFFER_SIZE = 10 * 2**20 # 10MB
+        filesList = dict()
+        duplicatedFiles = [] # TODO MAKE LIST OF DUPLICATES AND SHOW IT
+        includedExtensions = [".pdf", ".doc", ".docx", ".odt", ".mobi", ".epub", ".djvu", ".jpg", ".jpeg", ".png"]
+
+        for path2, subdirs, files in os.walk(path):
+            for name in files:
+                add = False
+
+                if name == self.DATABASE_FILE_NAME:
+                    continue
+
+                #for extension in includedExtensions:
+                #    if name.lower().endswith(extension):
+                #        add = True
+                #        break
+                add = True
+
+                if add:
+                    filePath = os.path.join(path2, name)
+                    with open(filePath, 'rb') as file:
+                        hash = hashlib.sha1()
+                        while True:
+                            data = file.read(HASH_BUFFER_SIZE)
+                            if not data:
+                                break
+                            hash.update(data)
+                        # TODO CHECK FOR DUPLICATES WITH FILE ON DISK AND RETURN INFO ABOUT THEM AND DON'T LOAD THEM
+                        filesList[hash.hexdigest()] = filePath.replace(path, "")
+
+        return filesList, duplicatedFiles
+
+    def _executeSql(self, sqlStatements):
+        result = True
+
+        try:
+            connection = sqlite3.connect(self.databasePath)
+
+            for statement in sqlStatements:
+                connection.cursor().execute(statement[0], statement[1])
+
+            connection.commit()
+        except Exception:
+            result = False
+            pass
+        finally:
+            connection.close()
+
+        return result
+
+    def isDirectory(self, path):
+        return os.path.isdir(path)
+
+    def loadItems(self, libraryPath):
+        if not self._pathEndsWithSlash(libraryPath):
+            libraryPath = libraryPath + "/"
+
+        self.databasePath = libraryPath + self.DATABASE_FILE_NAME
+
+        if not self._checkIfDatabaseExists(self.databasePath):
+            self._createDatabase(self.databasePath)
+
+        databaseItems = self._loadItemsFromDatabase(self.databasePath)
+        filesData, duplicatedFiles  = self._listFiles(libraryPath)
+
+        databaseHashes = {}
+        items = {} # {"fileHash" => [False, False]} - first one if hash exists in files, second if in database
+        sqlStatements = []
+
+        for fileHash, filePath in filesData.items():
+            items[fileHash] = [True, False]
+
+        for databaseItem in databaseItems:
+            data = items.get(databaseItem[2], [False, True])
+            data[1] = True
+            items[databaseItem[2]] = data
+            databaseHashes[databaseItem[2]] = databaseItem[1]
+
+        addedItems = 0
+        removedItems = 0
+        movedItems = 0
+
+        for fileHash, info in items.items():
+            if info[0] and not info[1]:
+                sqlStatements += [[self.INSERT_STATEMENT, {"relative_path": filesData[fileHash], "content_hash": fileHash}]]
+                addedItems += 1
+
+            if not info[0] and info[1]:
+                sqlStatements += [[self.DELETE_STATEMENT, {"content_hash": fileHash}]]
+                removedItems += 1
+
+            if info[0] and info[1]:
+                if filesData[fileHash] != databaseHashes[fileHash]:
+                    sqlStatements += [[self.UPDATE_PATH_STATEMENT, {"relative_path": filesData[fileHash], "content_hash": fileHash}]]
+                    movedItems += 1
+
+        self._executeSql(sqlStatements)
+
+        self.items = self._loadItemsFromDatabase(self.databasePath)
+
+        return self.items, addedItems, removedItems, movedItems
+
+    def getNumberOfItems(self):
+        return len(self.items)
+
+    def getItems(self):
+        return self.items
+
+    def getReadItems(self):
+        return 0 if not self.items else reduce((lambda a, b: a + b), list(map(lambda x: x[3], self.items)))
+
+    def changeReadState(self, position):
+        if len(self.items) <= 0:
+            return # TODO MAYBE CHECK OTHER WAY IF POSITION IS CORRECT?
+        
+        self.items[position] = (self.items[position][0], self.items[position][1], self.items[position][2], int(not self.items[position][3]))
+        sqlStatement = [[self.UPDATE_READ_STATUS_STATEMENT, {"is_read": self.items[position][3], "id": self.items[position][0]}]]
+        self._executeSql(sqlStatement)
---
+++ view.py
@@ -0,0 +1,213 @@
+import curses, curses.panel, curses.ascii
+
+class View:
+    STATUS_BAR = " e(x)it | (c)hange library path | (r)eload | (SPC) read/unread | Read items: {}/{}"
+    ITEMS_LIST_HEADER = " Number | Is read? | Path"
+
+    def __init__(self, screen):
+        self.libraryPath = ""
+        self.position = 0
+        self.startListPosition = 0
+        self.endListPosition = 0
+        self.screen = screen
+        self._initializeCurses()
+        self._createPanels()
+
+    def _initializeCurses(self):
+        self.screen.clear()
+        self.screen.refresh()
+        curses.curs_set(0)
+
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
+
+    def _createPanels(self):
+        self.windows = []
+        self.panels = []
+
+        self.windows.append(curses.newwin(0, 0, 0, 0)) # DB path
+        self.panels.append(curses.panel.new_panel(self.windows[0]))
+
+        self.windows.append(curses.newwin(0, 0, 0, 0)) # Info about refreshing (also if given path is not directory)
+        self.panels.append(curses.panel.new_panel(self.windows[1]))
+
+        self.windows.append(curses.newwin(0, 0, 0, 0)) # Info after reloading DB
+        self.panels.append(curses.panel.new_panel(self.windows[2]))
+
+        self.windows.append(curses.newwin(0, 0, 0, 0)) # Extensions window
+        self.panels.append(curses.panel.new_panel(self.windows[3]))
+
+        self.panels[0].hide()
+        self.panels[1].hide()
+        self.panels[2].hide()
+        self.panels[3].hide()
+
+    def _drawItems(self, items, selectedItem):
+        index = 1
+        for item in items:
+            if item == selectedItem:
+                self.screen.attron(curses.color_pair(1))
+            self.screen.addstr(index, 0, "{:^8}|{:^10}| {}".format(self.startListPosition + index, ("*" if item[3] == 1 else ""), item[1]))
+            if item == selectedItem:
+                self.screen.attroff(curses.color_pair(1))
+            index = index + 1
+
+    def getPressedCharacter(self):
+        return self.screen.getch()
+
+    def drawScreen(self, items, readItems):     
+        self.screen.clear()
+        self.height, self.width = self.screen.getmaxyx()
+
+        if len(items) > 0:
+            if (self.height - 2) != (self.endListPosition - self.startListPosition):
+                self.endListPosition = (self.startListPosition + (self.height - 3))
+                if self.position > self.endListPosition:
+                    self.endListPosition = self.position
+                    self.startListPosition = self.endListPosition - (self.height - 3)
+            if self.endListPosition >= len(items):
+                self.endListPosition = len(items) - 1
+                self.startListPosition = self.endListPosition - (self.height - 3)
+        
+        # Items list
+        self.screen.attron(curses.color_pair(1))
+        self.screen.addstr(0, 0, self.ITEMS_LIST_HEADER)
+        self.screen.addstr(0, len(self.ITEMS_LIST_HEADER), " " * (self.width - len(self.ITEMS_LIST_HEADER) - 1))
+        self.screen.attroff(curses.color_pair(1))
+        if len(items) > 0:
+            self._drawItems(items[self.startListPosition:(self.endListPosition + 1)], items[self.position])
+
+        # Status bar
+        statusBar = self.STATUS_BAR.format(readItems, len(items))
+        self.screen.attron(curses.color_pair(1))
+        self.screen.addstr(self.height - 1, 0, statusBar)
+        self.screen.addstr(self.height - 1, len(statusBar), ("library: " + self.libraryPath + " ").rjust((self.width - len(statusBar) - 1), " "))
+        self.screen.attroff(curses.color_pair(1))
+
+        curses.panel.update_panels()
+        self.screen.refresh()
+
+    def showLibrarySelectionDialog(self):
+        window = curses.newwin(3, int(self.width - 0.5 * self.width), int(self.height - 0.5 * self.height - 2), int(self.width - 0.75 * self.width))
+        window.attron(curses.color_pair(2))
+        window.box()
+        window.attroff(curses.color_pair(2))
+        self.windows[0] = window
+        self.panels[0].replace(window)
+        self.panels[0].top()
+        self.panels[0].show()
+
+        characterPressed = -1
+
+        while characterPressed != 10:
+            if characterPressed == curses.KEY_BACKSPACE:
+                self.libraryPath = self.libraryPath[:-1]
+            else:
+                try:
+                    self.libraryPath += chr(characterPressed)
+                except ValueError:
+                    pass
+
+            self.windows[0].attron(curses.color_pair(2))
+            self.windows[0].addstr(1, 1, "Path to library: " + self.libraryPath.ljust(int(self.width - 0.5 * self.width - 19), " "))
+            self.windows[0].attroff(curses.color_pair(2))
+            curses.panel.update_panels() # TODO IS THIS REALLY NECESSARY?
+            self.screen.refresh()
+
+            characterPressed = self.getPressedCharacter()
+
+        self.panels[0].hide()
+        curses.panel.update_panels()
+        self.screen.refresh()
+
+    def showReloadLibraryDialog(self):
+        window = curses.newwin(7, 24, int(self.height - 0.5 * self.height - 4), int(self.width / 2 - 12))
+        window.attron(curses.color_pair(2))
+        window.box()
+        window.addstr(1, 1, "".ljust(22, " "))
+        window.addstr(2, 1, "".ljust(22, " "))
+        window.addstr(3, 1, "     Reloading...     ")
+        window.addstr(4, 1, "".ljust(22, " "))
+        window.addstr(5, 1, "".ljust(22, " "))
+        window.attroff(curses.color_pair(2))
+        self.windows[1] = window
+        self.panels[1].replace(window)
+        self.panels[1].top()
+        self.panels[1].show()
+        curses.panel.update_panels()
+        self.screen.refresh()
+
+    def hideReloadLibraryDialog(self):
+        self.panels[1].hide()
+
+    def showReloadSummaryDialog(self, addedItems, removedItems, movedItems):
+        window = curses.newwin(9, 20, int(self.height - 0.5 * self.height - 5), int(self.width / 2 - 10))
+        window.attron(curses.color_pair(2))
+        window.box()
+        window.addstr(1, 1, "".ljust(18, " "))
+        window.addstr(2, 1, "       INFO       ")
+        window.addstr(3, 1, "".ljust(18, " "))
+        window.addstr(4, 1, (" " + str(addedItems) + " files added").ljust(18, " "))
+        window.addstr(5, 1, (" " + str(removedItems) + " files removed").ljust(18, " "))
+        window.addstr(6, 1, (" " + str(movedItems) + " files moved").ljust(18, " "))
+        window.addstr(7, 1, "".ljust(18, " "))
+        window.attroff(curses.color_pair(2))
+        self.windows[2] = window
+        self.panels[2].replace(window)
+        self.panels[2].top()
+        self.panels[2].show()
+        curses.panel.update_panels()
+        self.screen.refresh()
+
+        while self.getPressedCharacter() != 10:
+            continue
+
+        self.panels[2].hide()
+        self.screen.refresh()
+
+    def getLibraryPath(self):
+        return self.libraryPath
+
+    def moveDown(self, numberOfItems):
+        self.position = ((self.position + 1) % numberOfItems)
+        if self.position > self.endListPosition:
+            self.startListPosition = self.startListPosition + 1
+            self.endListPosition = self.endListPosition + 1
+        if self.position < self.startListPosition:
+            self.startListPosition = self.position
+            self.endListPosition = self.position
+
+    def moveUp(self, numberOfItems):
+        self.position = ((self.position - 1) % numberOfItems)
+        if self.position < self.startListPosition:
+            self.startListPosition = self.startListPosition - 1
+            self.endListPosition = self.endListPosition - 1
+        if self.position > self.endListPosition:
+            self.startListPosition = self.position
+            self.endListPosition = self.position
+
+    def moveScreenDown(self, numberOfItems):
+        delta = self.endListPosition - self.startListPosition
+        if (self.endListPosition + delta) > numberOfItems:
+            self.endListPosition = numberOfItems - 1
+            self.startListPosition = self.endListPosition - delta
+            self.position = self.endListPosition
+        else:
+            self.startListPosition = self.startListPosition + delta
+            self.endListPosition = self.endListPosition + delta
+            self.position = self.startListPosition
+
+    def moveScreenUp(self):
+        delta = self.endListPosition - self.startListPosition
+        if (self.startListPosition - delta) < 0:
+            self.startListPosition = 0
+            self.endListPosition = self.startListPosition + delta
+        else:
+            self.startListPosition = self.startListPosition - delta
+            self.endListPosition = self.endListPosition - delta
+        self.position = self.startListPosition
+
+    def getPosition(self):
+        return self.position
